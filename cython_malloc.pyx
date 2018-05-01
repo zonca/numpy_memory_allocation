5addd65b704eb74efbdbab15ed903a677fe85fb2
Andrea Zonca <code@andreazonca.com>
2018-05-01 09:13:34 -0700

    zero array

--- cython_malloc.pyx
+++ cython_malloc.pyx
@@ -4,5 +4,8 @@ from libc.stdlib cimport malloc, free
 def create_array_malloc(long number=1):
     # allocate number * sizeof(double) bytes of memory
     cdef double *my_array = <double *>malloc(number * sizeof(double))
+    cdef long i
+    for i in range(number):
+        my_array[i] = 0.
     if not my_array:
         raise MemoryError()
